701ec85ec2a2e3e6b77775a43c010fc6bcf95dce
Yanun1 <xachik.gasparyan99@gmail.com>
2023-10-11 18:44:56 +0400

    ordersi formi het popoxutyunneri skizb

--- views/site/orders.php
+++ views/site/orders.php
@@ -4,16 +4,29 @@
   use yii\helpers\Html;
 ?>
 <div class="newForm">
-<form  class="OrdersForm">
-<?php $form = ActiveForm::begin(['options' =>['id' =>'OrdersForm']]) ?>
-<?= $form->field($model,'products')->label('Products')?>
-<?= $form->field($model,'client')->label('Client')?>
-<?= $form->field($model,'price')->label('Price')?>
-<?= $form->field($model,'count')->label('Count')?>
-<?= $form->field($model,'sum')->label('Sum')?>
-<?= Html::button('+', ['class' =>'btn btn-success']) ?>
-<?= Html::button('-', ['class' =>'btn btn-danger']) ?>
- <?php ActiveForm::end() ?>
-</form>
+    <?php $form = ActiveForm::begin(['options' =>['class' =>'ordersMain']]) ?>
+        <div class="OrdersForm">
+            <?= $form->field($model,'products')->label('Products')?>
+            <?= $form->field($model,'client')->label('Client')?>
+            <?= $form->field($model,'price')->label('Price')?>
+            <?= $form->field($model,'count')->label('Count')?>
+            <?= $form->field($model,'sum')->label('Sum')?>
+            <?= Html::button('+', ['class' =>'btn btn-success']) ?>
+            <?= Html::button('-', ['class' =>'btn btn-danger']) ?>
+        </div>
+<!--        <?php //= //Html::submitButton('Order')  ?>-->
+    <?php ActiveForm::end() ?>
 </div>
 
+<style>
+    .OrdersForm {
+        display: flex;
+        gap: 2px;
+        align-items: end;
+    }
+
+    .OrdersForm > .btn {
+        height: 38px;
+        width: 50px;
+    }
+</style>
--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+    function removeForm() {
+        console.log('remove');
+        if($(".OrdersForm").length === 1) {
+            return;
+        }
+        $(this).parent().remove();
+    }
     function duplicateForm() {
+        console.log('duplicate');
         let newDiv = $(".OrdersForm:first-child").clone();
         newDiv.find('.btn-success').click(duplicateForm);
-        $(".newForm").append(newDiv);
-        // $(this).parent;
+        newDiv.find('.btn-danger').click(removeForm);
+        console.log($(".ordersMain"));
+        $(".ordersMain").append(newDiv);
     }
-    function removeForm(ev) {
-        console.log(ev);
-    }
+
     $(".btn-success").click(duplicateForm);
     $(".btn-danger").click(removeForm);
    
