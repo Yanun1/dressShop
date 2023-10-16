963604e4446d6a54c171311461ff736bc62bb4c1
Yanun1 <xachik.gasparyan99@gmail.com>
2023-10-16 11:48:19 +0400

    linkery stilery poxeci ordersi creati

--- components/views/menu_tpl/menu.php
+++ components/views/menu_tpl/menu.php
@@ -1,15 +1,15 @@
 <div class="category-products">
-<li value="<?= $category['id']?>" src="<?= $category['image']?>" >
-    <a href="#"> <?= $category['product'] ?></a>
-   <?php if(isset($category['childs'])):?>
-    <a class = "badge pull-right"><i class="fa fa-plus"></i></a>
-    <?php endif;?>
-   <?php if(isset($category['childs'])):?>
-    <ul>
-        <a href="#"><?= getMenuHtml($category['childs'])?> </a>
-    </ul>
-    <?php endif ?>
-</li>
+    <li value="<?= $category['id']?>" src="<?= $category['image']?>" >
+        <a href="#"> <?= $category['product'] ?></a>
+       <?php if(isset($category['childs'])):?>
+        <a class = "badge pull-right"><i class="fa fa-plus"></i></a>
+        <?php endif;?>
+       <?php if(isset($category['childs'])):?>
+        <ul>
+            <a href="#"><?= getMenuHtml($category['childs'])?> </a>
+        </ul>
+        <?php endif ?>
+    </li>
 </div>
 
 
@@ -20,15 +20,16 @@
         list-style-type: none;
    }
 
-    a{
-    cursor: pointer;
-    text-decoration:none;
-    color:black;
-    font-family: "Times New Roman", Times, serif;
-    font-weight:bold;
+    .category-products a{
+        cursor: pointer;
+        text-decoration:none;
+        color:black;
+        font-family: "Times New Roman", Times, serif;
+        font-weight:bold;
    }
+
    .dcjq-icon{
-    display:none;
+        display:none;
    }
 </style>
  
